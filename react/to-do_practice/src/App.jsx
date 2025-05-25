16bcae344062bd23ff6e98ebdcee2e80cc43ce03
Priyanshu Kumar Rai <Priyanshurai835@gmail.com>
2025-05-25 21:07:14 +0530

    tow way binding applied on form

--- react/to-do_practice/src/App.jsx
+++ react/to-do_practice/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [username, setUsername] = useState("")
+  const [age, setAge] = useState(1)
 
   return (
     <>
-     <h1>App</h1>
+      <h1>User Data</h1>
+
+      <form onSubmit={(e) => {e.preventDefault()}}>
+        <input onChange={(e)=>{setUsername(e.target.value)}} value={username} type="text" placeholder='Username'/>
+        <input onChange={(e)=>{setAge(e.target.value)}} value={age} type="number" placeholder='Age' />
+        <button>Submit</button>
+      </form>
+
     </>
   )
 }
