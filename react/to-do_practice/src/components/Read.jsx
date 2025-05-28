9a3c81f8933d551e7daab5222d787f76cca22461
Priyanshu Kumar Rai <Priyanshurai835@gmail.com>
2025-05-28 20:43:52 +0530

    todo deletion functionality added

--- react/to-do_practice/src/components/Read.jsx
+++ react/to-do_practice/src/components/Read.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 const Read = (props) => {
     const todo=props.todo;
     const settodo=props.settodo;
+    const DeleteHandler=(id)=>{
+      let filtertodo=todo.filter(todo => todo.id != id)
+      settodo(filtertodo);
+    }
     const rendertitle = todo.map((todo) => {
-    return <li key={todo.id}>{todo.title}</li>
+    return <li key={todo.id}>{todo.title} | <span onClick={()=>DeleteHandler(todo.id)}>Delete</span></li>
   })
   return (
     <>
